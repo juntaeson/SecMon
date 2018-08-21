2abf382c6e38832e84b1ef41a1df6714582ef01e
jtson <jtson@192.168.193.1>
2018-08-21 22:20:12 +0900

    final

--- secmon/MacSecurityAudit.py
+++ secmon/MacSecurityAudit.py
@@ -2,9 +2,9 @@ import re
 import subprocess
 import json
 from datetime import datetime
-from elasticsearch import Elasticsearch
+#from elasticsearch import Elasticsearch
 
-es = Elasticsearch(['http://192.168.0.13:9200'])
+#es = Elasticsearch(['http://192.168.0.13:9200'])
 
 
 class audit:
@@ -24,7 +24,13 @@ class audit:
     ####################### END #############################
     
     ############### AUDIT START ##############################
-    
+    def getVers(self): #1.0
+        ret = str(subprocess.check_output("sw_vers", shell=True))
+        rProductName = re.compile(r"ProductName:(.+?)$")
+        rProductVersion = re.compile(r"ProductVersion:(.+?)$")
+        rBuildVersion = re.compile(r"BuildVersion:(.+?)$")
+        return rProductVersion.search(ret).group(1)
+     
     def getUpdateHistory(self): # 1-1  update history
         output = str(subprocess.check_output('softwareupdate --history', shell=True))
         output = output.split("\\n")
@@ -42,27 +48,23 @@ class audit:
         return json.dumps(updates)
     
     def getAutoUpdate(self): #1-2
-        output = subprocess.check_output('defaults read /library/preferences/com.apple.commerce', shell=True)
+        output = str(subprocess.check_output('defaults read /library/preferences/com.apple.commerce', shell=True))
         pAutoUpdate = re.compile(r'(?<=AutoUpdate\s=\s)\d')
         AutoUpdate = pAutoUpdate.search(output)
         return AutoUpdate.group()
         #appUpdate Check disable 0, check enable 1
 
-    def getJavaVersion(self): # 1-3 java -version
-        output = subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT)
-        mjavav = pJavav.search(output)
-        if mjavav == None:
-            java = {
-                "javav" : "None"
-            }
-        else: 
-            java = {
-                "javav" : mjavav.group()
-            }
-        return json.dumps(java)
+    def getjava(self): # 1-3 taylor
+        output = subprocess.check_output(['java', '-version'],stderr=subprocess.STDOUT)
+        pjava = re.compile(r"java version \"(\d+\.\d+\.\d+)\"")
+        mjava = pjava.search(output)
+        if mjava != None:
+            return mjava.group(1)
+        else:
+            return 'None'
     
-    def getGatekeeper(self): #2-0
-        output = subprocess.check_output('spctl --status', shell=True)
+    def getGatekeeper(self): #2-0 kyumm
+        output = str(subprocess.check_output('spctl --status', shell=True))
         pStatus = re.compile("enable")
         mStatus = pStatus.search(output)
         if mStatus.group() == "enable":
@@ -70,7 +72,7 @@ class audit:
         else: 
             return 0
     
-    def getTime(self): # 2-3
+    def getTime(self): # 2-3 taylor
         output = str(subprocess.check_output('systemsetup -getusingnetworktime',shell=True))
         if output.find("On") != -1:
             return 1
@@ -84,8 +86,8 @@ class audit:
             else:
                     return 1
                 
-    def getRemoteLogin(self): #2-7
-        output = subprocess.check_output('sudo systemsetup -getremotelogin', shell=True)
+    def getRemoteLogin(self): #2-7 kyumm
+        output = str(subprocess.check_output('sudo systemsetup -getremotelogin', shell=True))
         pRlogin = re.compile(r'(?<=Remote\sLogin:\s)\w*')
         Rlogin = pRlogin.search(output)
         if Rlogin.group() == "Off":
@@ -93,15 +95,16 @@ class audit:
         else: 
             return 0      
     
-    def askForPW(self): # 2-10
+    def askForPW(self): # 2-10 taylor
         output = str(subprocess.check_output('defaults read com.apple.screensaver askForPassword',shell=True))
         if output.find('0') != -1:
             return 0
         else:
             return 1
         
-    def getWebSandbox(self): #2-11
+    def getWebSandbox(self): #2-11 kyumm
         output = subprocess.check_output('defaults read com.apple.Safari AutoOpenSafeDownloads', shell=True)
+        #output = subprocess.check_output(['defaults','read','com.apple.Safari','AutoOpenSafeDownloads'], stderr=subprocess.STDOUT)
         pWebsandbox = re.compile("\d")
         Websandbox = pWebsandbox.search(output)
         #if Websandbox == "1":
@@ -118,7 +121,7 @@ class audit:
                     return 0
                 
     def isBluetoothMode(self): #3-0-1
-            output = str(subprocess.check_output('/usr/sbin/system_profiler SPBluetoothDataType | grep –i discoverable', shell=True))
+            output = str(subprocess.check_output('/usr/sbin/system_profiler SPBluetoothDataType | grep -i discoverable',shell=True))
             if output.find("off") == -1:
                     return 0
             else:
@@ -129,7 +132,7 @@ class audit:
             hotspot = re.compile('(.+?(\d)){,2}')
             return hotspot.search(output).group(2)
             
-    def isBluetoothShare(self): # 3-0-3
+    def isBluetoothShare(self): # 3-0-3 taylor
         output = str(subprocess.check_output('/usr/sbin/system_profiler SPBluetoothDataType | grep State',shell=True))
         pShare = re.compile(r"^.+(?<=State:\s)(.+?)$", re.MULTILINE)
         mShare = pShare.search(output)
@@ -138,7 +141,7 @@ class audit:
         else: 
             return 0
 
-    def isInternettShare(self): # 3-1
+    def isInternettShare(self): # 3-1 taylor
         output = str(subprocess.check_output('defaults read /Library/preferences/SystemConfiguration/com.apple.nat | grep -i Enabled',shell=True))
         if output.find('1') != -1:
             return 1
@@ -147,7 +150,7 @@ class audit:
         
     def isOnAppFileServer(self): # 3-2
             #launchctl list | egrep AppleFileServer output = str(subprocess.check_output('',shell=True)
-            output = str(subprocess.check_output('launchctl list | egrep AppleFileServer',shell=True))
+            output = str(subprocess.check_output('launchctl list | grep AppleFileServer',shell=True))
             if output.find('0') != -1:
                     return 1 # 0 if there is '0' in result, AppFileServer is On
             else:            # as we discussed, 1 dose mean turn on
@@ -167,7 +170,7 @@ class audit:
             else:
                     return 0
                 
-    def getHomeDirPermission(self): #4-1 
+    def getHomeDirPermission(self): #4-1  kyumm
         output = subprocess.check_output('ls -l /Users/ | egrep -iE -v "root|administrator|total"', shell=True)
         pUsers = re.compile(r'(\D{12})(\d+)(\D+\d+)', re.MULTILINE)
         #output = output.split()
@@ -177,8 +180,24 @@ class audit:
             return mUsers.group(1), mUsers.group(3)
         else:
             return 0
-
-    def getAccountLockThreshold(self): #4-4 
+        
+    def AppDirPermission(self): # 4-2 taylor
+        output=str(subprocess.check_output('sudo find /applications -iname "*\.app" -type d -perm -2 -ls',shell=True))
+        if output == ' ':
+            return 1
+        else:
+            return 0
+        
+    def SystemDirPermission(self): # 4-3 taylor
+        output=str(subprocess.check_output('sudo find /System -type d -perm -2 -ls | grep -v "Public/Drop Box"',shell=True))
+        #output=str(subprocess.check_output('sudo find /System -type d -perm -2 -ls | grep -v "p'))
+        
+        if output == ' ':
+            return 1
+        else:
+            return 0
+        
+    def getAccountLockThreshold(self): #4-4  kyumm
         output = subprocess.check_output('pwpolicy -getglobalpolicy', shell=True)
         Ppwpolicy = re.compile(r"(.+?)$")
         Mpwpolicy = Ppwpolicy.search(output)
@@ -188,7 +207,7 @@ class audit:
         else:
             return 0
 
-    def AutoLogin(self): # 4-6
+    def AutoLogin(self): # 4-6 taylor
         output=str(subprocess.check_output('defaults read /library/preferences/com.apple.loginwindow',shell=True))
         pUser = re.compile(r"autoLoginUser\s\=\s(\w+?)\;")
         mUser = pUser.search(output)
@@ -197,8 +216,15 @@ class audit:
         else:
             return 'None'
         
-    def getUseGuestAccount(self): #4-8
-        output = subprocess.check_output('Defaults read /library/preferences/com.apple.loginwindow.plist GuestEnabled', shell=True)
+    def RequirePasswordForSystem(self): # 4-7 taylor
+        output=str(subprocess.check_output('security authorizationdb read system.preferences 2> /dev/null | grep -A1 shared | grep -E "(true|false)"',shell=True))
+        if output.find('true') != -1:    # true :  no require / false = require
+            return 0
+        else:
+            return 1
+
+    def getUseGuestAccount(self): #4-8 kyumm
+        output = str(subprocess.check_output('Defaults read /library/preferences/com.apple.loginwindow.plist GuestEnabled', shell=True))
         #pGuest = re.compile("0")
         #mGuest = pGuest.search(output)
         if output.find('0') != -1:
@@ -214,7 +240,6 @@ class audit:
 #test  = audit()
 #print(test.getIP())
 
-
 aud = audit()
 IP, MAC = aud.getIPnMAC()
 result = {
@@ -222,15 +247,47 @@ result = {
         'mac':MAC,
         'time': datetime.now(),
         'AudidtResult' :     {
-             "-1-CurrentOSVersion" : getHostName()
+            # "A-1-CurrentOSVersion" : aud.getVers(),
+             "A-2-LastOSUpdate" : aud.getUpdateHistory(),
+             "A-3-AutomaticUpdate" : aud.getAutoUpdate(),
+             "A-4-JavaVersion" : aud.isScreenShareOn(),
+             
+             "B-1-Gatekeeper" : aud.getGatekeeper(),
+             "B-2-PWPolicy" : "aaaa", #######
+             "B-3-Time" : aud.getTime(), 
+             "B-4-ScreenSleep" : "", #########
+             "B-5-ScreenShare" : aud.isScreenShareOn(),
+             "B-6-RemoteLogin" : aud.getRemoteLogin(),
+             "B-7-ScreenAuth" : aud.askForPW(),
+             "B-8-WebSandbox" : aud.getWebSandbox(),
+             
+             "C-1-Bluetooth" : aud.isBluetoothOn(),
+             "C-1-Bluetooth-Mode" : aud.isBluetoothMode(),
+             "C-1-Bluetooth-Hotspot" : aud.isBluetoothHotspot(),
+             "C-1-Bluetooth-share" : aud.isBluetoothShare(),
+             "C-2-InternetSharing" : aud.isInternettShare(),
+             #"C-3-FileSharing" : aud.isOnAppFileServer(),
+             "C-4-Firewall" : "a", #########
+             "C-5-DropICMP" : aud.isFWOn(),
+             
+             "D-1-HomeDirPermission" : aud.getHomeDirPermission(),
+             "D-2-AppDirPermission" : aud.AppDirPermission(),
+             "D-3-SystemDirPermission" : aud.SystemDirPermission(),
+             "D-4-AccountLockThreshold" : aud.getAccountLockThreshold(),
+             "D-5-UseRootAccount" : "aa",   #########
+             "D-6-UseAutoLogin" : aud.AutoLogin(),
+             "D-7-RequirePasswordForSystem" : aud.RequirePasswordForSystem(),
+             "D-8-UseGuestAccount" : aud.getUseGuestAccount()
+             
             }
     }
 
+print(result)
 #es = es.delete(index="test-index", doc_type='audit-result',id=1)
 #es.reindex(body=result)
 #res = es.index(index="test-index", doc_type='audit-result1',id=1,body=result)
 #print(res['result'])
 
-res = es.get(index="test-index", doc_type='_all',id=1)
+#res = es.get(index="test-index", doc_type='_all',id=1)
 #res = es.mget(index="test-index",body=1)
-print(res['_source']) 
+#print(res['_source']) 
